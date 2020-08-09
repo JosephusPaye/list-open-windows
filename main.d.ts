ae1b1dd2f267eae3e0f1f7af21ded8092082aa39
Josephus Paye II <j.paye96@gmail.com>
2020-08-09 18:49:00 +1000

    Add comments to WindowInfo interface in types file

--- main.d.ts
+++ main.d.ts
@@ -1,9 +1,32 @@
 export interface WindowInfo {
+    /**
+     * The window handle (HWND)
+     */
     handle: number;
+
+    /**
+     * The window position in z-order
+     */
     zOrder: number;
+
+    /**
+     * The window class name, as retrieved from [GetClassName](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getclassname)
+     */
     className: string;
+
+    /**
+     * The window title/caption, as retrieved from [GetWindowText](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowtexta)
+     */
     caption: string;
+
+    /**
+     * The id of the thread that the window belongs to, as retrieved from [GetWindowThreadProcessId](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowthreadprocessid)
+     */
     processId: number;
+
+    /**
+     * The path to the executable of the process that the window belongs to, as retrieved from [GetModuleFileNameEx](https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-getmodulefilenameexa)
+     */
     processPath: string;
 }
 
